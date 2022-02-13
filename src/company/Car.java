82b143caca8f3b9d578f4c6798a370552852159c
Fabian13PL <fabian13pl@gmail.com>
2022-02-13 12:30:28 +0100

    Task 5

--- src/company/Car.java
+++ src/company/Car.java
@@ -3,9 +3,16 @@ package company;
 public class Car {
     final String model;
     final String producer;
+    private Double value;
 
     public Car(String model, String producer) {
         this.model = model;
         this.producer = producer;
+    }
+    public Double getValue(){
+        return value;
+    }
+    public void setValue(Double value){
+        this.value=value;
     }
 }
