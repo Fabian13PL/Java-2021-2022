6f93b9b1500944f7a49ac50d6fb224328453661e
Fabian13PL <fabian13pl@gmail.com>
2022-02-17 20:06:32 +0100

    Task 12

--- src/company/devices/Car.java
+++ src/company/devices/Car.java
@@ -1,8 +1,46 @@
 package company.devices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import company.Human;
 
 public abstract class Car extends Devices {
+
+    List<Human> owners = new ArrayList<>();
+
+    public void addOwner(Human owner) {
+        owners.add(owner);
+    }
+
+    public Human owner() {
+        owners.size();
+        return owners.get(owners.size() - 1);
+    }
+
+    public boolean isThisHumanWasOwner(Human human) {
+        for (Human owner : owners) {
+            if (owner == human)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean sellerAndBuyer(Human seller, Human buyer) {
+        for (int i = 0; i < owners.size(); i++) {
+            if (seller == owners.get(i)) {
+                if (owners.get(i + 1) == buyer) {
+                    return true;
+                }
+                break;
+            }
+        }
+        return false;
+    }
+
+    public int howManyOwners() {
+        return owners.size();
+    }
 
     public Double getValue() {
         return value;
@@ -33,6 +71,10 @@ public abstract class Car extends Devices {
                 throw new Exception("seller don't have this car");
         }
 
+        if (seller.getCar(sellerCar).owner() != seller) {
+            throw new Exception("seller is not owner of this car");
+        }
+
         for (int i = 0; i < buyer.garageSize(); i++) {
             if (seller.getCar(i) == null) {
                 sellerCar = i;
@@ -41,13 +83,15 @@ public abstract class Car extends Devices {
                 throw new Exception("buyer don't have space for new car");
         }
 
-        if (buyer.cash < price)
+        if (buyer.cash < price) {
             throw new Exception("buyer don't have cash");
+        }
 
         buyer.cash -= price;
         seller.cash += price;
         buyer.setCar(seller.getCar(sellerCar), buyerCar);
         seller.deleteCar(sellerCar);
+        buyer.getCar(buyerCar).addOwner(buyer);
         System.out.println(seller + " sold " + buyer.getCar(buyerCar) + " to " + buyer + " for " + price);
 
     }
